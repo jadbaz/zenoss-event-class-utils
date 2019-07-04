fab8e96004b8363ac0285635779b1497b63f8281
Jad Baz <jad.baz@cardinalhealth.com>
2019-07-04 15:39:59 +0300

    Fixed script directory

--- zenoss_list_and_export_event_classes.sh
+++ zenoss_list_and_export_event_classes.sh
@@ -1,3 +1,7 @@
+SCRIPT_NAME="$(basename $0)"
+SCRIPT_FULL_PATH="$(readlink -f $0)"
+DIR="$(dirname ${SCRIPT_FULL_PATH})"
+
 pip install pyyaml --user
 
 basename="zenoss_event_classes_`date +'%Y_%m_%d_%H_%M_%S'`"
@@ -6,13 +10,13 @@ out_csv="${basename}.csv"
 
 echo "Started extracting event classes to ${out_yml}"
 echo "*************************************"
-time zendmd --script zenoss_list_event_classes.py | tail -n +2 > ${out_yml}
+time zendmd --script ${DIR}/zenoss_list_event_classes.py | tail -n +2 > ${out_yml}
 echo "Finished extracting event classes to ${out_yml}"
 
 echo "*************************************"
 
 echo "Started exporting event classes to ${out_csv}"
 echo "*************************************"
-time python2.7 zenoss_export_event_classes_csv.py ${out_yml} ${out_csv}
+time python2.7 ${DIR}/zenoss_export_event_classes_csv.py ${out_yml} ${out_csv}
 echo "Finished exporting event classes to ${out_csv}"
 
